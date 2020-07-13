9772e189116874cc9d6eb3f15aff5a0a311ec31d
Alexei Colin <acolin@isi.edu>
2020-07-13 22:16:19 +0000

    apps: matrix: wrapper harness for Halide kernel
    For testing.

---
+++ apps/matrix/bright.cpp
@@ -0,0 +1,18 @@
+#include "HalideBuffer.h"
+#include "halide_bright.h"
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+	Halide::Runtime::Buffer<double> input(3, 2), output(3, 2);
+	int offset = 0;
+	struct halide_buffer_t *input_buf =
+		static_cast<struct halide_buffer_t *>(input);
+	int error = halide_bright(offset, input, output);
+	if (error) {
+		printf("Halide kernel error: %d\n", error);
+		return 1;
+	}
+	return 0;
+}
