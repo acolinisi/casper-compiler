6ccb5cb385893f94f4965ec9158709378a5d571c
Alexei Colin <acolin@isi.edu>
2020-07-16 00:20:24 +0000

    app: matrix: blur: parametrized schdule

--- apps/matrix/halide_kern.meta.cpp
+++ apps/matrix/halide_kern.meta.cpp
@@ -19,6 +19,7 @@
 
 #include "Halide.h"
 #include <stdio.h>
+#include <cmath>
 
 using namespace Halide;
 
@@ -81,29 +82,8 @@ public:
     }
 };
 
-enum class BlurGPUSchedule {
-    Inline,          // Fully inlining schedule.
-    Cache,           // Schedule caching intermedia result of blur_x.
-    Slide,           // Schedule enabling sliding window opt within each
-                     // work-item or cuda thread.
-    SlideVectorize,  // The same as above plus vectorization per work-item.
-};
-
-std::map<std::string, BlurGPUSchedule> blurGPUScheduleEnumMap() {
-    return {
-        {"inline", BlurGPUSchedule::Inline},
-        {"cache", BlurGPUSchedule::Cache},
-        {"slide", BlurGPUSchedule::Slide},
-        {"slide_vector", BlurGPUSchedule::SlideVectorize},
-    };
-};
-
 class HalideBlur : public Halide::Generator<HalideBlur> {
 public:
-    GeneratorParam<BlurGPUSchedule> schedule{
-        "schedule",
-        BlurGPUSchedule::SlideVectorize,
-        blurGPUScheduleEnumMap()};
     GeneratorParam<int> tile_x{"tile_x", /* 32 */ 1};  // X tile.
     GeneratorParam<int> tile_y{"tile_y", /* 8 */ 1};   // Y tile.
 
@@ -118,72 +98,66 @@ public:
         blur_x(x, y) = (input(x, y) + input(x + 1, y) + input(x + 2, y)) / 3;
         blur_y(x, y) = (blur_x(x, y) + blur_x(x, y + 1) + blur_x(x, y + 2)) / 3;
 
-        // How to schedule it
-        if (get_target().has_gpu_feature()) {
-            // GPU schedule.
-            switch (schedule) {
-            case BlurGPUSchedule::Inline:
-                // - Fully inlining.
-                blur_y.gpu_tile(x, y, xi, yi, tile_x, tile_y);
-                break;
-            case BlurGPUSchedule::Cache:
-                // - Cache blur_x calculation.
-                blur_y.gpu_tile(x, y, xi, yi, tile_x, tile_y);
-                blur_x.compute_at(blur_y, x).gpu_threads(x, y);
-                break;
-            case BlurGPUSchedule::Slide: {
-                // - Instead caching blur_x calculation explicitly, the
-                //   alternative is to allow each work-item in OpenCL or thread
-                //   in CUDA to calculate more rows of blur_y so that temporary
-                //   blur_x calculation is re-used implicitly. This achieves
-                //   the similar schedule of sliding window.
-                Var y_inner("y_inner");
-                blur_y
-                    .split(y, y, y_inner, tile_y)
-                    .reorder(y_inner, x)
-                    .unroll(y_inner)
-                    .gpu_tile(x, y, xi, yi, tile_x, 1);
-                break;
-            }
-            case BlurGPUSchedule::SlideVectorize: {
-                // Vectorization factor.
-                int factor = sizeof(int) / sizeof(short);
-                Var y_inner("y_inner");
-                blur_y.vectorize(x, factor)
-                    .split(y, y, y_inner, tile_y)
-                    .reorder(y_inner, x)
-                    .unroll(y_inner)
-                    .gpu_tile(x, y, xi, yi, tile_x, 1);
-                break;
-            }
-            default:
-                break;
-            }
-        } else if (get_target().features_any_of({Target::HVX_64, Target::HVX_128})) {
-            // Hexagon schedule.
-            const int vector_size = get_target().has_feature(Target::HVX_128) ? 128 : 64;
-
-            blur_y.compute_root()
-                .hexagon()
-                .prefetch(input, y, 2)
-                .split(y, y, yi, 128)
-                .parallel(y)
-                .vectorize(x, vector_size * 2);
-            blur_x
-                .store_at(blur_y, y)
-                .compute_at(blur_y, yi)
-                .vectorize(x, vector_size);
-        } else {
-            // CPU schedule.
+        // CPU schedule.
 #if 0
-            blur_y.split(y, y, yi, 8).parallel(y).vectorize(x, 8);
-            blur_x.store_at(blur_y, y).compute_at(blur_y, yi).vectorize(x, 8);
+        blur_y.split(y, y, yi, 8).parallel(y).vectorize(x, 8);
+        blur_x.store_at(blur_y, y).compute_at(blur_y, yi).vectorize(x, 8);
 #else
-            blur_y.split(y, y, yi, 2).parallel(y).vectorize(x, 2);
-            blur_x.store_at(blur_y, y).compute_at(blur_y, yi).vectorize(x, 2);
-#endif
-        }
+#if 0
+        blur_y.split(y, y, yi, 2).parallel(y).vectorize(x, 2);
+        blur_x.store_at(blur_y, y).compute_at(blur_y, yi).vectorize(x, 2);
+#else
+        Var x_i("x_i");
+        Var x_i_vi("x_i_vi");
+        Var x_i_vo("x_i_vo");
+        Var x_o("x_o");
+        Var x_vi("x_vi");
+        Var x_vo("x_vo");
+        Var y_i("y_i");
+        Var y_o("y_o");
 
+#if 0
+        int power = 10;
+        int p1 = rand() % power + 1;
+        int p2 = rand() % power + 1;
+        int p3 = rand() % p2 + 1; // p2 > p3
+        int p4 = rand() % p3 + 1; // p3 > p4
+#else
+        int p1 = 1;
+        int p2 = 1;
+        //int p3 = 1 + 1; // p3 > p2
+        //int p4 = 1 + 1; // p4 > p3
+        int p3 = 1;
+        int p4 = 1;
+#endif
+
+        int v1 = pow(2,p1);
+        int v2 = pow(2,p2);
+        int v3 = pow(2,p3);
+        int v4 = pow(2,p4);
+
+        {
+            Var x = blur_x.args()[0];
+            blur_x
+                .compute_at(blur_y, x_o)
+                .split(x, x_vo, x_vi, v1)
+                .vectorize(x_vi);
+        }
+        {
+            Var x = blur_y.args()[0];
+            Var y = blur_y.args()[1];
+            blur_y
+                .compute_root()
+                .split(x, x_o, x_i, v2)
+                .split(y, y_o, y_i, v3)
+                .reorder(x_i, y_i, x_o, y_o)
+                .split(x_i, x_i_vo, x_i_vi, v4)
+                .vectorize(x_i_vi)
+                .parallel(y_o)
+                .parallel(x_o);
+        }
+#endif
+#endif
 
         // Match mem layout to Casper Dat buffers (see comments in the other
         // generator)
