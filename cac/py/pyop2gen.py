8f7ad8109dca543129c18363234bbba2a10bfb8b
Alexei Colin <acolin@isi.edu>
2020-11-15 20:54:39 -0500

    cac/py: automatically add parloops from solver

--- cac/py/pyop2gen.py
+++ cac/py/pyop2gen.py
@@ -3,9 +3,17 @@ import importlib
 def codegen(gen_module, gen_func):
     gen_mod = importlib.import_module(gen_module)
     print(f"codegen {gen_module}.{gen_func}")
-    loops = gen_mod.generate()
-    for task, loops in loops.items():
-        print(f"task {task}: {len(loops)} loops")
-        for loop in loops:
-            if hasattr(loop, "compute"): # some are funcs
-                loop._jitmodule
+    explicit_loops, solver = gen_mod.generate()
+    implicit_loops = dict(
+            _jac=solver._ctx._assemble_jac,
+            _residual=solver._ctx._assemble_residual
+            )
+    if solver._ctx.Jp is not None:
+        implicit_loops["_pjac"]=solver._ctx._assemble_pjac
+
+    for loop_set in [explicit_loops, implicit_loops]:
+        for task, loops in loop_set.items():
+            print(f"task {task}: {len(loops)} loops")
+            for loop in loops:
+                if hasattr(loop, "compute"): # some are funcs
+                    loop._jitmodule
